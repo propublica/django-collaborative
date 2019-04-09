72f756a200e5061d87b371fee2f0146651f702ff
Brandon Roberts <brandon@bxroberts.org>
2019-04-09 16:34:01 -0700

    Auto-register model admin, show logentry admin

--- collaborative/admin.py
+++ collaborative/admin.py
@@ -1,4 +1,8 @@
+from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.admin.models import LogEntry
+from django.apps import apps
+
 
 UserAdmin.list_display = ("username","email","first_name","last_name")
 # UserAdmin.list_editable = ("first_name", "last_name")
@@ -7,3 +11,18 @@ UserAdmin.add_fieldsets = ((None, {
     'fields': ('username', 'email', 'password1', 'password2'),
     'classes': ('wide',)
 }),)
+
+admin.site.register(LogEntry)
+
+# Auto-register Admins for all models. This avoids needing to
+# write code and inject it into an admin.py for our auto-generated
+# models
+models = apps.get_models()
+for model in models:
+    if "collaborative.models" not in str(model):
+        continue
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
+
