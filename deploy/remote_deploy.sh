5889df36e30d38132b94eb9cd8cfcdd95143c4c2
Brandon Roberts <brandon@bxroberts.org>
2019-08-07 13:58:27 -0700

    Capture and rotate data refreshing cron logs

--- deploy/remote_deploy.sh
+++ deploy/remote_deploy.sh
@@ -101,8 +101,11 @@ sudo mv -f /opt/collaborative/app/deploy/cron/refresh_data_sources \
     || die "Failure to copy data refreshing cron script"
 sudo chown root:root /etc/cron.daily/refresh_data_sources \
     || die "Failure to set ownership of data refreshing cron script"
-sudo chmod 755 /etc/cron.daily/refresh_data_sources \
+sudo chmod 644 /etc/cron.daily/refresh_data_sources \
     || die "Failure to set perms on data refreshing cron script"
+sudo mv -f /opt/collaborative/app/deploy/logrotate/refresh_data_sources \
+    /etc/logrotate.d/refresh_data_sources \
+    || die "Failure to copy data refreshing logrotate config"
 
 # Apply everything
 sudo systemctl restart apache2
