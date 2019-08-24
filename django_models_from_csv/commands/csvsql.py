8605d749fdd2dd1fbedf5e25a30a542ca16fc698
Brandon Roberts <brandon@bxroberts.org>
2019-08-23 18:04:14 -0700

    Disable inference, due to problems with psql

--- django_models_from_csv/commands/csvsql.py
+++ django_models_from_csv/commands/csvsql.py
@@ -50,7 +50,7 @@ class FakeArgs():
     no_constraints = False
     no_create = False
     no_header_row = False
-    no_inference = False
+    no_inference = True
     overwrite = False
     prefix = []
     query = None
