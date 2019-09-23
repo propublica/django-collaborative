6353592a21cd8d9792edac2b889173292d0bd4d4
Brandon Roberts <brandon@bxroberts.org>
2019-09-23 15:45:38 -0700

    Sorting and binding mostly work together
    One remaining issue: Add new works if you haven't re-ordered
    anything. But once you've re-ordered and then add new, the new
    element gets pushed one up from the last item, as opposed to
    actually at the end of the list. This is despite the fact that
    the data model actually has the correct ordering, so this may
    be a race condition between the two modules.

--- django_models_from_csv/static/django_models_from_csv/columnswidget.js
+++ django_models_from_csv/static/django_models_from_csv/columnswidget.js
@@ -1,5 +1,17 @@
 // Rivets.js
 (() => {
+  const dbgColumns = (d) => {
+    d.columns.forEach((c, ix) => {
+      console.log("Column", ix, c.ix, c.name, c.type);
+    });
+  };
+
+  // TODO: update the textarea with the state of the columns
+  const updateState = (d) => {
+    console.info("updateState");
+    dbgColumns(d);
+  };
+
   // TODO: fetch columns data from API or injected onto window
   const data = {
     columns: [{
@@ -28,11 +40,11 @@
       data.columns.splice(scope.index, 1);
     },
     onchange: (ev, scope) => {
-      console.log("onchange ev", ev, "scope", scope);
-      // TODO: update the textarea with the state of the columns
+      updateState(data);
     },
-    ondrag: (ev, scope) => {
-      console.log("ondrag ev", ev, "scope", scope);
+    swapitems: (a, b) => {
+      console.log("a", a, "b", b);
+      console.log("this", this);
     },
   };
 
@@ -46,28 +58,57 @@
     };
   };
 
+  const sortCSS = "table#render-meta_columns tbody";
+
   const startSortable = () => {
-    const sortCSS = "table#render-meta_columns tbody";
     const options = {
       items: 'tr',
       forcePlaceholderSize: true,
     };
+
     const s = sortable(sortCSS, options);
     s[0].addEventListener('sortupdate', function(e) {
-      console.log("sortable update", e);
+      console.warn("sortable update data");
+      const start = e.detail.origin.index;
+      const end = e.detail.destination.index;
+      console.log("start", start, "end", end);
+      //const dest = data.columns[end];
+      //data.columns[end] = data.columns[start];
+      //data.columns[start] = dest;
+      data.swapitems(start, end);
+      console.warn("sortupdate complete");
+      dbgColumns(data);
     });
+
+    console.warn("startSortable complete");
+    dbgColumns(data);
   };
 
+  let bound = null;
   const main = () => {
-    rivets.bind($("#render-meta_columns"), data);
+    bound = rivets.bind($("#render-meta_columns"), data);
     startSortable();
   };
 
   // add blank field, this is outside of the scope of the
   // bound rivets object so we attach it to the window
   window.COLLABaddField = (widgetSelector) => {
+    console.warn("Adding field");
     const widget = $(widgetSelector);
-    data.columns.push(makeDefaultColumn());
+    // re-enable the sortable to get the new field to work. we
+    // need to start sorting *after* we mutate the columns list.
+    sortable(sortCSS, "destroy");
+    bound.unbind();
+    data.columns.push({
+      ix: data.columns.length,
+      name: '',
+      type: 'text',
+      value: null,
+      filterable: false,
+      searchable: false,
+    });
+    bound = rivets.bind($("#render-meta_columns"), data);
+    startSortable();
   };
 
   $(document).ready(main);
